b5828344b6d786d5a79df194b1e9b074734624db
hmAAmh <DatFido@yahoo.com>
2022-05-23 00:36:42 -0700

    Made jump system more robust

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -3,35 +3,80 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         super(scene, x, y, texture, frame);
         this.scene = scene;
         this.walkTime = 0;
+        this.jumped = false;
+        this.moveAnim = false;
         scene.add.existing(this);
         console.log(this);
+
+        this.anims.create({
+            key: 'playerRun',
+            frames: this.anims.generateFrameNumbers('playerRun', {frames: [0, 1, 2]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+
 
     }
     update() {
 
-        if(Phaser.Input.Keyboard.JustDown(keySPACE)) {
+        if(!(this.jumped) && Phaser.Input.Keyboard.JustDown(keySPACE) && (this.body.blocked.down)) {
             this.setVelocityY(-500);
             this.scene.sound.play('sfx_jump');
+            this.setTexture('playerRise');
+            this.jumped = true;
+            this.moveAnim = false;
+            this.anims.stop();  
+
+            //this.emitter.setPosition(this.x, this.y);
+            //this.emitter.explode();     
         }
-        else if (keySPACE.isDown && keySPACE.getDuration() <= 250) {
+        else if(this.jumped && keySPACE.isDown && keySPACE.getDuration() <= 250 && !(this.body.blocked.down)) {
+            this.setTexture('playerRise');
             this.setVelocityY(-500);
+            this.anims.stop(); 
         }
+        else if(this.jumped && (!(keySPACE.isDown) || keySPACE.getDuration() > 350)){
+            this.jumped = false;
+            this.setTexture('playerFall');
+            this.moveAnim = false;
+        }
+        else if(this.jumped && this.body.blocked.down){
+            this.jumped = false;
+            if(keyRIGHT.isDown || keyLEFT.isDown) {
+                this.anims.play('playerRun');
+            }
+        }
+
+
         if(keyRIGHT.isDown) {
             this.setVelocityX(200);
             this.walkTime++;
             this.flipX = false;
+            if(!(this.moveAnim) && this.body.blocked.down) {
+                this.moveAnim = true;
+                this.anims.play('playerRun');
+            }
         }
         else if(keyLEFT.isDown) {
             this.setVelocityX(-200);
             this.walkTime++;
             this.flipX = true;
+            if(!(this.moveAnim) && this.body.blocked.down) {
+                this.moveAnim = true;
+                this.anims.play('playerRun');
+            }
         }
         else {
             this.setVelocityX(0);
-            this.walkTime = 0;
+            this.walkTime = 0; 
+            if((this.body.blocked.down)){
+               this.setTexture('player');
+               this.moveAnim = false;
+            }
         }
 
-        if(this.walkTime % 20 == 1){
+        if(this.walkTime % 20 == 1 && this.body.blocked.down){
             this.scene.sound.play('sfx_walk');
         }
         //console.log(keySPACE.getDuration());
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,10 +3,18 @@ class Play extends Phaser.Scene {
         super("play");
     }
     preload () {
-        //Visual
+        //Sprites
         this.load.image('tile', './assets/images/Tile.png');
         this.load.image('player', './assets/images/Player.png');
+        this.load.image('playerRise', './assets/images/playerRise.png');
+        this.load.image('playerFall', './assets/images/playerFall.png');
         this.load.image('door', './assets/images/door.png');
+
+        //Animation
+        this.load.spritesheet('playerRun', './assets/animations/playerWalk.png', {frameWidth: 32, frameHeight: 64, startFrame: 0, endFrame: 1});
+
+        //Particles
+        this.load.image('grayPart', 'assets/images/particle.png');
 
         //Audio
         this.load.audio('sfx_jump', './assets/sfx/sfx_jump.ogg');
@@ -23,9 +31,26 @@ class Play extends Phaser.Scene {
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         
-        this.player = new Player(this, 200, 600, 'player').setOrigin(0,0);
+        this.playerEmitter = this.add.particles('grayPart').createEmitter({
+            x: 400,
+            y: 300,
+            speed: { min: -800, max: 800 },
+            angle: { min: 0, max: 360 },
+            scale: { start: 0.5, end: 0 },
+            //blendMode: 'SCREEN',
+            //active: false,
+            lifespan: 500,
+            frequency: -1,
+            quantity: 50
+        }); 
+
+        this.player = new Player(this, 200, 600, 'player', this.playerEmitter).setOrigin(0,0);
         let playerGroup = this.physics.add.group([this.player]);
         this.player.setFriction(1);
+        
+
+
+        this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true).setGravityY(2000);
         this.bgRight = new Block(this, -32 , -32, 'tile', undefined, 32, 22, true);
         this.block1 = new Block(this, 320, 18 * this.pixelSize, 'tile', undefined, 5, 2, false);
@@ -40,6 +65,7 @@ class Play extends Phaser.Scene {
         this.mainCamera.startFollow(this.player);
         this.mainCamera.setDeadzone(200,200);
 
+        //Tweens
         this.idleTween = this.tweens.add({
             targets: this.player,
             scaleX: (0.9, 1.1),
@@ -48,6 +74,8 @@ class Play extends Phaser.Scene {
             repeat: -1,
             yoyo: true
         });
+
+
     }
     update() {
         if (this.checkCollisionDoor(this.door1)) {
